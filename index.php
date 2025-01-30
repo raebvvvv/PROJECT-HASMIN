8ceb13e66168bf8ba3b051b602a88912c1670ec0
mark Garcia <markreinier.garcia@gmail.com>
2025-01-30 22:14:17 +0800

    .

--- index.php
+++ index.php
@@ -7,8 +7,13 @@ if (!isset($_SESSION['user_id'])) {
     // Redirect to the login page if not logged in
     header("Location: login.php");
     exit();
+} else {
+    // Redirect to index.php if the user is logged in
+    header("Location: index.php");
+    exit();
 }
-?> 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- user_contact.php
+++ user_contact.php
@@ -31,6 +31,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $error = "Error: " . $e->getMessage();
     }
 }
+
+
 ?>
 
 <!DOCTYPE html>
