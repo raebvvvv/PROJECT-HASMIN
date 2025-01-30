c73224b90bccedbf1451c0b01022c49d5abebd6b
mark Garcia <markreinier.garcia@gmail.com>
2025-01-30 19:33:26 +0800

    try

--- login.php
+++ login.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link hre="login.css" rel="stylesheet">
+  <link href="login.css" rel="stylesheet">
   <link rel="stylesheet" href="style.css">
   <title>Hasmin</title>
 </head>
@@ -69,8 +69,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <br>
     <a href="register.php"><button class="btn btn-secondary mt-3">Register</button></a>
     
-    <!-- Button with Modal -->
-    <a href="register.php"><button type="button" class="btn btn-secondary mt-3">Create an Account</button></a>
+  
   </div>
   
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
